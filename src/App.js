bb423a19851d565edc1456b717e0c49b5e9c42af
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-20 19:57:50 -0800

    city explorer setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Header from './header.js';
-import Footer from './footer.js';
-import Main from './main.js';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
-      <div className="App">
+      <>
         <Header />
         <Main />
         <Footer />
-      </div>
+      </>
     );
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -1,49 +1,31 @@
 import React from 'react';
-import Map from './map.js';
-import Restaurants from './restaurants.js';
-import location from './fake-data/location.json';
-import restaurants from './fake-data/restaurants.json';
-import map from './images/map.png';
 
 class Main extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      displayResults: false,
-      locationObj: location,
-      restaurants: restaurants
-    }
+      city: ''
+    };
   }
 
-  handleLocationSearch = (e) => {
-    e.preventDefault();
-    this.setState({ displayResults: true });
-  }
+  handleCityInput = (e) => {
+    this.setState({
+      city: e.target.value
+    })
+  };
 
-  render(){
-    return(
-      <div id="main">
-        <form onSubmit={this.handleLocationSearch} id="search-form">
-          <label>Search for a location</label>
-          <input type="text" name="search" id="input-search" placeholder="Enter a location here" />
-          <button type="submit">Explore!</button>
+  render() {
+    console.log('app state: ', this.state)
+    return (
+      <>
+        <form>
+          <label>Pick a City!!!
+            <input type="text" onInput={this.handleCityInput}/>
+          </label>
+          <button>Lets Go!</button>
         </form>
-
-        {this.state.displayResults &&
-          <div>
-            <Map
-              location={this.state.locationObj}
-              map={map}
-            />
-            <Restaurants
-              restaurants={this.state.restaurants}
-              location={this.state.locationObj}
-            />
-          </div>
-        }
-
-      </div>
-    )
+      </>
+    );
   }
 }
 
